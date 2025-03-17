ba140a22f33ba3e5523b59ef200be2fe6ba8de5a
lokanadham-devops1994 <lokanadham.mec1994@gmail.com>
2025-03-17 15:33:05 +0530

    Javacode (#2)
    * Java Test code file
    
    * Java Test code file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Welcome to IPL March22nd 2025:!");
+    System.out.println("Welcome to IPL March22nd 2025!");
   }
 }
